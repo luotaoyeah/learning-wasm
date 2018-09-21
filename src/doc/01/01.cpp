dd50c34e619c828fea4cd7dec193b16bf7c769ca
luotao <luotaoyeah@gmail.com>
2018-09-21 20:51:30 +0800

    hello world

---
+++ src/doc/01/01.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ *
+ * @return
+ */
+int main() {
+    /*
+     * source ../../juj/emsdk/emsdk_env.sh --build=Release
+     */
+    printf("hello world\n");
+    return 0;
+}
