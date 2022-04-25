e0d7144c142f273c485960ac96939bd1fb47a65e
luotao <luotaoyeah@gmail.com>
2022-04-25 22:26:39 +0800

    feat(learning-wasm-pack): cargo generate --git https://github.com/rustwasm/wasm-pack-template

--- projects/rust/learning-wasm-pack/src/lib.rs
+++ projects/rust/learning-wasm-pack/src/lib.rs
@@ -1,6 +1,12 @@
 use wasm_bindgen::prelude::*;
 use web_sys::console;
 
+mod utils;
+
+#[cfg(feature = "wee_alloc")]
+#[global_allocator]
+static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+
 /// 问候.
 #[wasm_bindgen]
 pub fn log(name: &str) {
